a39ba046c1781dcca9f5fcb4683855be9263cfb9
EmcTROJ1N <gerkatv@mail.com>
2021-07-25 20:34:17 +0300

    update

--- CPP/bulls and cows.cpp
+++ CPP/bulls and cows.cpp
@@ -10,27 +10,27 @@ int isPerm (long num1, long num2) {
     int digits[10];
     int i;
 
-    for (i = 0; i < 10; i++)      // Init all counts to zero.
+    for (i = 0; i < 10; i++)    
         digits[i] = 0;
 
-    while (num1 != 0) {           // Process all digits.
-        digits[num1%10]++;        // Increment for least significant digit.
-        num1 /= 10;               // Get next digit in sequence.
+    while (num1 != 0) {          
+        digits[num1%10]++;       
+        num1 /= 10;              
     }
 
-    while (num2 != 0) {           // Same for num2 except decrement.
+    while (num2 != 0) {          
         digits[num2%10]--;
         num2 /= 10;
     }
 
     for (i = 0; i < 10; i++)
-        if (digits[i] != 0)       // Any count different, not a permutation.
+        if (digits[i] != 0)       
             return false;
 
-    return true;                  // All count identical, was a permutation.
+    return true;                  
 }
 
-bool isRepeat(int number) {
+bool isRepeatNumb(int number) {
     std::set<int> d;
     int counter = 0;
     
@@ -43,30 +43,41 @@ bool isRepeat(int number) {
     return counter != d.size();
 }
 
+bool isRepeatNumbs(int num1, int num2)
+{
+    int lDigit;
+    for (int j = 0, k = 1000; j < 4; j++, k /= 10)
+        {
+            int tmpVar = num1;
+            while (tmpVar != 0)
+            {
+                lDigit = tmpVar % 10;
+                tmpVar /= 10;
+                if (lDigit == (num2 / k) % 10)
+                    return true;
+            }
+        }
+        return false;
+}
+
 int main()
 {
     srand(time(0));
-    int counter = 1;
+    int counter = 0;
 
     vector<int> numbers;
+    vector<int> lNumbers;
 
     for (int i = 1000, j = 0; j < 5040; j++, i++) // Заполнение вектора
     {
-        if (isRepeat(i) == false)
+        if (isRepeatNumb(i) == false)
             numbers.push_back(i);
     }
-   
-    for (int i = 1111; i < 9999; i += 1111)
-    {
-        numbers.erase(remove(numbers.begin(), numbers.end(), i), numbers.end());
-    }
-
-    int index = rand() % numbers.size();
-    int number[4]; //Генерация случайного числа
-    //int numb = rand() % (9999 - 1000 + 1) + 1000;
-
-    int numb = 1254;
-
+    
+    int number[4]; 
+    
+    //Генерация случайного числа
+    int numb = numbers[rand() % numbers.size()];
     int tmpVarNumb = numb;
     for (int i = 3; i >= 0; i--)
     {
@@ -77,40 +88,38 @@ int main()
     int sumCountCoincidences = 0;
     int sumCountCoincidencesPos = 0;
 
+    int lcountCoincidencesPos;
+    int lcountCoincidences;
     int countCoincidencesPos;
     int countCoincidences;
     int numbTwo;
+    int lNumb;
+    int index;
+    bool flager = false;
+
     while (true)
     {
+        counter++;
         if (counter == 1) numbTwo = 1234;
-        if (counter == 2) numbTwo = 5678; 
-        if (counter == 3)
+        if (counter == 2) numbTwo = 5678;
+       if (counter >= 3)
         {
-            if (sumCountCoincidencesPos + sumCountCoincidences >= 4)
+            if (flager == false)
             {
-                int tmpVar;
-                int lDigit;
-                for (int i = 0; i < numbers.size(); i++)
+                index = -1;
+                do
                 {
-                    tmpVar = numbers[i];
-                    while (tmpVar != 0)
-                    {
-                        lDigit = tmpVar % 10;
-                        tmpVar /= 10;
-                        if (lDigit == 9 || lDigit == 0)
-                        {
-                            numbers.erase(remove(numbers.begin(), numbers.end(), numbers[i]), numbers.end());
-                            i--;
-                            break;
-                        }
-                    }
-                }
+                    index++;
+                } while (isRepeatNumbs(numbers[index], lNumb));
+                numbTwo = numbers[index];
+            }
+            else
+            {
+                index = rand() % numbers.size();
+                numbTwo = numbers[index];
             }
         }
-        if (counter == 4)
-            cin >> numbTwo;
-        //cin >> numbTwo;
-
+        
         int numberTwo[4];
         tmpVarNumb = numbTwo;
 
@@ -119,6 +128,7 @@ int main()
         {
             numberTwo[i] = tmpVarNumb % 10; 
             tmpVarNumb /= 10;
+            lNumbers.push_back(numberTwo[i]);
         }
 
         countCoincidencesPos = 0;
@@ -144,8 +154,13 @@ int main()
         // Проверка на угаданное число
         if (countCoincidencesPos == 4)
         {
-            cout << "Krasavcheg";
+            cout << endl << numb << endl;
+            cout << "Krasavcheg" << endl;
             break;
+        }
+        else
+        {
+            numbers.erase(remove(numbers.begin(), numbers.end(), numbTwo), numbers.end());
         }
         
 
@@ -164,7 +179,7 @@ int main()
             }
         }
 
-        if ((countCoincidences == 2 && countCoincidencesPos == 2) || (countCoincidences == 3 && countCoincidencesPos == 1))
+        if ((countCoincidences == 2 && countCoincidencesPos == 2) || (countCoincidences == 3 && countCoincidencesPos == 1) || (countCoincidences == 4))
         {
             numbers.erase(remove(numbers.begin(), numbers.end(), numbTwo), numbers.end());
             for (int i = 0; i < numbers.size(); i++)
@@ -182,7 +197,8 @@ int main()
             {
                 if (numbers[i] == numb)
                 {
-                    cout << "Krasavcheg.";
+                    cout << endl << numb << endl;
+                    cout << "Krasavcheg." << endl;
                     flag = true;
                     break;
                 }
@@ -193,14 +209,57 @@ int main()
             }
         }
         
-
-        if (counter <= 2)
+        if (flager == false)
         {
             sumCountCoincidencesPos += countCoincidencesPos;
             sumCountCoincidences += countCoincidences;
         }
         
-        counter++;
-        cout << "Vector size: " << numbers.size() << endl;
+        if (sumCountCoincidences + sumCountCoincidencesPos >= 4)
+        {
+                flager = true;
+                int lDigit;
+                int tmpVar;
+                for (int i = 0; i < numbers.size(); i++)
+                {
+                    bool flag = true;
+                    tmpVar = numbers[i];
+                    while (tmpVar != 0)
+                    {
+                        lDigit = tmpVar % 10;
+                        tmpVar /= 10;
+                        if (find(lNumbers.begin(), lNumbers.end(), lDigit) != lNumbers.end() == false)
+                        {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if (flag == false)
+                    {
+                        numbers.erase(remove(numbers.begin(), numbers.end(), numbers[i]), numbers.end());
+                        i--;
+                    }
+                }
+
+        } 
+        if (counter % 2 == 0)
+        {
+            lNumbers.clear();
+            sumCountCoincidences = 0;
+            sumCountCoincidencesPos = 0;
+        }
+
+        if (flager == true)
+        {
+            sumCountCoincidences = 0;
+            sumCountCoincidencesPos = 0;
+        }
+        cout << "Vector size: " << numbers.size() << " " << endl;
+        
+        lNumb = numbTwo;
+        lcountCoincidences = countCoincidences;
+        lcountCoincidencesPos = lcountCoincidencesPos;
     } 
+
+    cout << "Counter: " << counter << endl << endl;
 }
