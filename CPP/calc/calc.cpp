447158e795848903b4db27b16bd1b324235bfb4f
EmcTROJ1N <gerkatv@gmail.com>
2021-09-20 17:16:51 +0300

    add new homework

--- CPP/calc/calc.cpp
+++ CPP/calc/calc.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -22,6 +23,11 @@ vector<string> split(const string& str, const string& delim)
 int calc(int a, int b, char symb)
 {
     int res;
+    if (symb == '/' && b == 0)
+    {
+        cout << "Invalid input";
+        exit(0);
+    }
     switch (symb)
     {
         case '+': res = a + b; break;
@@ -32,12 +38,54 @@ int calc(int a, int b, char symb)
     return res;
 }
 
+
+bool isValid(string N)
+{
+    bool flag = true;
+    for (int i = 0; i < N.size(); i++)
+    {
+        if (i != 0)
+        {
+            if (N[i] == N[i - 1])
+            {
+                if (!((int)N[i] > 48 && (int)N[i] < 57))
+                    flag = false;
+            }
+        }
+    }
+    for (int i = 0; i < N.size(); i++)
+    {
+        if (!((int)N[i] > 48 && (int)N[i] < 57 && N[i] != '*' && N[i] != '/' && N[0] != '-' && N[0] != '+'))
+            flag = false;
+    }
+    if (N.size() == 1)
+    {
+        if (N[0] == '-' || N[0] == '+' || N[0] == '*' || N[0] == '/')
+            flag = false;
+    }
+
+    if (N.size() == 0)
+        flag = false;
+
+    return flag;
+}
+
+
 void task_1()
 {
     string N;
     vector<string> numbers;
     vector<char> symbols;
     getline(cin, N);
+    
+    if (N.size() == 1)
+    {
+        if (N[0] == '+' || N[0] == '-' || N[0] == '/' || N[0] == '*')
+        {
+            cout << "Invalid input";
+            exit(0);
+        }
+    }
 
     for (int i = 0; i < N.size(); i++)
     {
@@ -51,34 +99,64 @@ void task_1()
     numbers = split(N, "+");
 
     int sum = 0;
-    vector<int> deletedIndexes;
     
     for (int i = 0; i < symbols.size(); i++)
     {
         if (symbols[i] == '/' || symbols[i] == '*')
         {
-            int res = calc(stoi(numbers[i]), stoi(numbers[i + 1]), symbols[i]);
-            numbers.erase(numbers.begin() + i + 1);
-            deletedIndexes.push_back(i);
-            symbols.erase(symbols.begin() + i);
-            numbers[i] = to_string(res);
-            i--;
+            if (isValid(numbers[i]) && isValid(numbers[i + 1]))
+            {
+                cout << "Invalid input";
+                exit(0);
+            }
+            else
+            {
+                int res = calc(stoi(numbers[i]), stoi(numbers[i + 1]), symbols[i]);
+                numbers.erase(numbers.begin() + i + 1);
+                symbols.erase(symbols.begin() + i);
+                numbers[i] = to_string(res);
+                i--;
+            }
         }
     }
     cout << endl;
-    
     if (numbers.size() != 1)
     {
         for (int i = 0; i < symbols.size(); i++)
         {
             if (i == 0)
-                sum = calc(stoi(numbers[i]), stoi(numbers[i + 1]), symbols[i]);
+            {
+                if (isValid(numbers[i]) && isValid(numbers[i + 1]))
+                    sum = calc(stoi(numbers[i]), stoi(numbers[i + 1]), symbols[i]);
+                else
+                {
+                    cout << "Invalid input";
+                    exit(0);
+                }
+            }
             else
-                sum = calc(sum, stoi(numbers[i + 1]), symbols[i]);
+            {
+                if (isValid(to_string(sum)) && isValid(numbers[i + 1]))
+                    sum = calc(sum, stoi(numbers[i + 1]), symbols[i]);
+                else
+                {
+                    cout << "Invalid input";
+                    exit(0);
+                }
+            }
         }
     }
     else
-        sum = stoi(numbers[0]);
+    {
+        if (isValid(numbers[0]))
+            sum = stoi(numbers[0]);
+        else
+        {
+            cout << "Invalid input";
+            exit(0);
+        }
+    }
+    
     cout << sum;
 }
 
