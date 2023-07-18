d01aefad02c8bc0ee007d2b4eea1ae6f109ce15b
German <58856791+EmcTROJ1N@users.noreply.github.com>
2023-07-19 00:24:16 +0300

    update homework

---
+++ ASP.NET/FileExplorer/FileExplorer/App_Code/CarouselItemHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FileExplorer;
+
+public static class CarouselItemHelper
+{
+    public static IDisposable CarouselItem(this IHtmlHelper html, string imgPath)
+    {
+        string beginHtml = $@"
+            <div class=""single-hero-item set-bg"" data-setbg=""{imgPath}"">
+                <div class=""container"">
+                    <div class=""row"">
+                        <div class=""col-lg-12"">
+                            <div class=""hero-text"">
+        ";
+        string endHtml = @"
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ";
+        html.ViewContext.Writer.Write(beginHtml);
+        return new SectionWrapper(endHtml, html.ViewContext.Writer);
+    }
+}
+
+public class SectionWrapper : IDisposable
+{
+    private readonly string ClosingStr;
+    private readonly TextWriter Writer;
+    public SectionWrapper(string closingStr, TextWriter writer)
+    {
+        this.ClosingStr = closingStr;
+        this.Writer = writer;
+    }
+    public void Dispose() =>
+        this.Writer.Write(this.ClosingStr);
+}
